5b033a8a5bf8d3e5a8cc4f12192de286610da66a
yinshuxun <sxyin@alauda.io>
2021-08-12 21:41:03 +0800

    add files

---
+++ projects/my-lib/src/public-api.ts
@@ -0,0 +1,9 @@
+/*
+ * Public API Surface of my-lib
+ */
+// import { DialogSize } from "@alauda/ui"
+// import { Translation } from '@alauda/common-snippet';
+import { DragRange } from '@alauda/common-snippet';
+
+// export const Big: DialogSize = DialogSize.Big
+export const range: DragRange = 1;
